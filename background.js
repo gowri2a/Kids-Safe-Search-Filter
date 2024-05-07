050cb1fb726fab3da5e18749a623f5f14e59e49d
Jessy <146324633+Jessyjes2003@users.noreply.github.com>
2024-05-07 09:01:06 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,70 @@
+//URLs to block
+let blockedUrls = [
+  {
+    link: "liquidgeneration.com",
+  },
+  {
+    link: "onlineanime.org",
+  },
+  {
+    link: "fanworkrecs.com",
+  },
+  {
+    link: "freecartoonsex.com",
+  },
+  {
+    link: "taremeparadise.com",
+  },
+  {
+    link: "hentaifree.net",
+  },
+  {
+    link: "jasonmeador.com",
+  },
+  {
+    link: "cutepet.org",
+  },
+  {
+    link: "shokushu.com",
+  },
+  {
+    link: "hentai-comics.com",
+  },
+  {
+    link: "jastusa.com",
+  },
+  {
+    link: "peachprincess.com",
+  },
+  {
+    link: "g-collections.com",
+  },
+  {
+    link: "canadahentai.com",
+  },
+  {
+    link: "hirameki-int.com",
+  },
+  {
+    link: "jlist.com",
+  },
+  {
+    link: "himeyashop.com",
+  },
+  {
+    link: "pcrdist.com",
+  },
+  {
+    link: "hentaigameszone.com",
+  },
+  {
+    link: "pureyaoi.org",
+  },
+];
+
+blockedUrls.forEach((element) => {
+  if (window.location.origin.includes(element.link)) {
+    document.getElementsByTagName("body")[0].innerHTML =
+      '<div><img src="https://i.ibb.co/BC9yWNx/backgroundimg.jpg" style="margin: 0px auto; height: 100vh; display: flex;"></div>';
+  }
+});
